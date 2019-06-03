bdf9841e0acf1d75c533403371bf5ed31e11fc38
cynhw <cynhowong@gmail.com>
2019-06-02 22:25:23 -0700

    Did a bunch of stuff in the back to start API calls

--- src/Parks.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export const Parks = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.city),
-    React.createElement("h2", {}, props.state)
-  ]);
-};
