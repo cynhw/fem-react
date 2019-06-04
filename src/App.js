f1d2940426cd7f33e640404dee9fbfbb055fde87
cynhw <cynhowong@gmail.com>
2019-06-03 21:13:35 -0700

    Using axios to fetch data from the NPS API

--- src/App.js
+++ src/App.js
@@ -3,24 +3,56 @@ import { render } from "react-dom";
 import axios from "axios";
 import { Park } from "./Park";
 
-const nationalpark = {key: process.env.API_KEY};
-console.log(nationalpark);
+const nps = process.env.API_KEY;
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      parks: []
+    };
+  }
 
   componentDidMount() {
+    axios
+      .get(
+        "https://developer.nps.gov/api/v1/parks?stateCode=WA&fields=name&fields=&api_key=" +
+          `${nps}`
+      )
 
+      // https://css-tricks.com/using-data-in-react-with-the-fetch-api-and-axios/
+      .then(res =>
+        res.data.data.map(park => ({
+          description: `${park.description}`,
+          fullname: `${park.fullName}`,
+          states: `${park.states}`
+        }))
+      )
+      .then(parks => {
+        this.setState({
+          parks
+        });
+      });
   }
 
   render() {
     return (
       <div>
-        <h1 onClick={this.handleTitleClick}>National Parks</h1>
-        <Park name="Olympic National Park" city="Port Angeles" state="Washington" />
-        <Park name="North Cacades National Park" city="Marblemount" state="Washington" />
-        <Park name="Mount Rainier National Park" city="Seattle" state="Washington" />
+        <h1>National Parks</h1>
+        <div>
+          { this.state.parks.map(park => {
+            return (
+              <Park 
+                name={park.fullname}
+                state={park.states}
+                description={park.description}
+              />
+            )
+          }) }
+        </div>
       </div>
-    );
+    )
   }
 }
 
