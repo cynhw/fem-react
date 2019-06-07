f7c2693c39d4d10b48a102983f98c4f626a90d5a
cynhw <cynhowong@gmail.com>
2019-06-06 20:42:00 -0700

    Moved the code from App.js to Results.js for routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
 import Results from './Results';
+import Details from './Details';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>National Parks</h1>
-        <Results />
+        <Link to="/">
+          <h1>National Parks</h1>
+        </Link>
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
